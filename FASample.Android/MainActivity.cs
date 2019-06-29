e6cf56d9a1ba70ae1e924a54ff119c881fcc8f38
gnk263 <gnk263@users.noreply.github.com>
2019-06-29 19:43:35 +0900

    send event for android

--- FASample.Android/MainActivity.cs
+++ FASample.Android/MainActivity.cs
@@ -7,6 +7,8 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Firebase.Analytics;
+
 namespace FASample.Droid
 {
     [Activity(Label = "FASample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -17,11 +19,20 @@ namespace FASample.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
+            var analytics = FirebaseAnalytics.GetInstance(this);
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            var bundle = new Bundle();
+            bundle.PutString(FirebaseAnalytics.Param.ItemCategory, "Monday");
+            bundle.PutString(FirebaseAnalytics.Param.ItemName, "21:54");
+            analytics.LogEvent(FirebaseAnalytics.Event.SelectContent, bundle);
+
+            analytics.SetCurrentScreen(this, "top screen", null);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
