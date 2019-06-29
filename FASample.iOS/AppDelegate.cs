6a4b5ffcee4ed714316e472c45aca4d2f18e596a
gnk263 <gnk263@users.noreply.github.com>
2019-06-30 00:12:58 +0900

    add EventLog

--- FASample.iOS/AppDelegate.cs
+++ FASample.iOS/AppDelegate.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
+using Firebase.Analytics;
+
 namespace FASample.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
@@ -25,8 +27,31 @@ namespace FASample.iOS
             // https://github.com/xamarin/GoogleApisForiOSComponents/issues/158#issuecomment-483194061
             var foo = Firebase.Core.Configuration.SharedInstance;
 
+            Firebase.Core.App.Configure();
+
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
+
+            var events = new Dictionary<string, string>
+            {
+                {
+                    ParameterNamesConstants.ItemCategory,
+                    "Friday"
+                },
+                {
+                    ParameterNamesConstants.ItemName,
+                    "17:08"
+                },
+            };
+
+            var sendParams = NSDictionary<NSString, NSObject>.FromObjectsAndKeys(
+                events.Values.ToArray(),
+                events.Keys.ToArray()
+            );
+
+            Analytics.LogEvent(EventNamesConstants.SelectContent, sendParams);
+
+            Analytics.SetScreenNameAndClass("main ios screen", this.ToString());
 
             return base.FinishedLaunching(app, options);
         }
