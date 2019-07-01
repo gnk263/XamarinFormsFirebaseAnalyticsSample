a8588da3cfef8f7cf529af464d27a7d31d553669
gnk263 <gnk263@users.noreply.github.com>
2019-07-01 22:29:49 +0900

    add Interface

---
+++ FASample/IAnalytics.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FASample
+{
+    public interface IAnalytics
+    {
+        void LogEvent(string eventName, Dictionary<string, string> eventParams);
+    }
+}
